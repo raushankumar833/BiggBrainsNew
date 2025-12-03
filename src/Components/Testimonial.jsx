8dd3deea0363c6f1e7c7ddf475f779e273741674
Aryan Singh <68284783+aryan1203@users.noreply.github.com>
2025-12-03 17:58:53 +0530

    chnages

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -5,7 +5,7 @@ const testimonials = [
   {
     name: "Anjali Verma",
     role: "Product Manager, TechCorp",
-    img: "https://images.unsplash.com/photo-1603415526960-f7e0328d88b0?w=600&q=80",
+    img: "https://images.unsplash.com/photo-1764592358977-a181a6e71af4?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     testimonial:
       "The team delivered beyond our expectations. Their strategy and execution are simply top-notch.",
     rating: 5,
